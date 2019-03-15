bab7dae566b2a11e32cfb58c26fbe50d8710144e
jknowles14 <julieanne.m.knowles@gmail.com>
2019-03-15 16:06:49 -0600

    Checkpoint DOM script events part2 push

--- app.js
+++ app.js
@@ -1,36 +1,39 @@
-function onReady(){
- const addToDoForm = document.getElementById('addToDoForm');
- const newToDoText = document.getElementById('newToDoText');
- const toDoList = document.getElementById('toDoList');
+function onReady() {
+   const toDos = []; //Our state is going to be an array of to-dos.
+   const addToDoForm = document.getElementById('addToDoForm');
+ function createNewToDo() { //  update our array of to-dos
+   const newToDoText = document.getElementById('newToDoText'); //access the text input, so that we can get the text for the title of each to-do.
+ if (!newToDoText.value) { return; } //prevents empty to do
 
- addToDoForm.addEventListener('submit', event => {
-   event.preventDefault();
+   toDos.push({ //add the new to-do to the toDos array using the push() method.
+         title: newToDoText.value,// assign the value of the text input, newToDoText to the title key
+         complete: false
+   });
+   newToDoText.value = ''; //clear the text input for the user.
 
-   //get the text
-   let title = newToDoText.value;
+    renderTheUI();//this is called everytime the state changes
+  }
+  function renderTheUI() { //will take our current state (the toDos array) and render the UI.
+      const toDoList = document.getElementById('toDoList'); // to access the <ul> in the HTML.
+      toDoList.textContent = '';
+    toDos.forEach(function(toDo) { //Using forEach is how we'll render each to-do as a li in the ul.
+      const newLi = document.createElement('li'); //next 3 lines are for checkbox
+      const checkbox = document.createElement('input');
+    checkbox.type = "checkbox";
 
-   // create a new li
-   let newLi = document.createElement('li');
+    newLi.textContent = toDo.title; //todos title duh
 
-   //create a new input
-   let checkbox = document.createElement('input');
-
-   //set the input's type to checkbox
-   checkbox.type = "checkbox";
-
-   //set the title
-   newLi.textContent = title;
-
-   //attach the checkbox to the li
-   newLi.appendChild(checkbox);
-
-   // attach the li to the ul
-   toDoList.appendChild(newLi);
-
-   //empty the input
-   newToDoText.value = '';
- });
+    toDoList.appendChild(newLi); // the DOM
+      newLi.appendChild(checkbox);
+    });
 }
+  addToDoForm.addEventListener('submit', event => { // event listener, using the submit event of the form element.
+    event.preventDefault();//prevent the page from reloading.
+    createNewToDo();//call the createNewToDo() function we just created.
+  });
+  renderTheUI(); //render the UI based on the current state.
+}
+//calls on Ready
 window.onload = function() {
   onReady();
 };
